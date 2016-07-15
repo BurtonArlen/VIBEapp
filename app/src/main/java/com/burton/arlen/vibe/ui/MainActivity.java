6f36f18ad883380dd2557093a37889fb2298ab07
arlen <burton.arlen.1990@gmail.com>
2016-07-15 09:04:37 -0700

    setup constants file for yelp api

--- app/src/main/java/com/burton/arlen/vibe/ui/MainActivity.java
+++ app/src/main/java/com/burton/arlen/vibe/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.burton.arlen.vibe;
+package com.burton.arlen.vibe.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.burton.arlen.vibe.R;
 
 public class MainActivity extends AppCompatActivity {
 
