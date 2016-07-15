d04d1f4a95539251db7f70d20694362d30716900
arlen <burton.arlen.1990@gmail.com>
2016-07-15 09:23:32 -0700

    gen yelp tokens

---
+++ app/src/main/java/com/burton/arlen/vibe/model/Spot.java
@@ -0,0 +1,7 @@
+package com.burton.arlen.vibe.model;
+
+/**
+ * Created by arlen on 7/15/16.
+ */
+public class Spot {
+}
---
+++ app/src/main/java/com/burton/arlen/vibe/serve/YelpServe.java
@@ -0,0 +1,7 @@
+package com.burton.arlen.vibe.serve;
+
+/**
+ * Created by arlen on 7/15/16.
+ */
+public class YelpServe {
+}
